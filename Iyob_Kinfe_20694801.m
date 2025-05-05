10b9012df6486f91c8e98c8d53e5d3b09e2a26e2
muhdin129 <egyik3@nottingham.ac.uk>
2025-05-05 17:51:30 +0100

    first file

---
+++ Iyob_Kinfe_20694801.m
@@ -0,0 +1,33 @@
+% IYOB KINFE
+% egyik3@nottingham.ac.uk
+
+
+%% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [10 MARKS]
+a = arduino;
+writeDigitalPin(a, 'D10', 1);
+
+%% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+
+% Insert answers here
+
+%% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
+
+% Insert answers here
+
+
+%% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [25 MARKS]
+
+% Insert answers here
+
+
+%% TASK 4 - REFLECTIVE STATEMENT [5 MARKS]
+
+% Insert reflective statement here (400 words max)
+
+
+%% TASK 5 - COMMENTING, VERSION CONTROL AND PROFESSIONAL PRACTICE [15 MARKS]
+
+% No need to enter any answershere, but remember to:
+% - Comment the code throughout.
+% - Commit the changes to your git repository as you progress in your programming tasks.
+% - Hand the Arduino project kit back to the lecturer with all parts and in working order.
